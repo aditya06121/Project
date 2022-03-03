4481cc45ea5e6b3601de1e1cb66a3f31e1b98adc
Aditya <69010952+aditya06121@users.noreply.github.com>
2022-03-03 22:55:25 +0530

    reference updated

--- Python Codes/Basics/16-Program for removing repeating item from string.py
+++ Python Codes/Basics/16-Program for removing repeating item from string.py
@@ -1,1 +1,6 @@
 x = [1,1,4,5,5,7,7,8,9,9,10]
+x.sort # it sorts the list for further manupilation
+print (x)
+y = x # second list for operations
+for z in x:
+    print(z)
---
+++ Python Codes/Basics/README.md
@@ -0,0 +1,2 @@
+well... this is the directory containing all the basic python program files
+i wonder when will i graduate from basics 26 - sat - feb over and out
